69095ce83e110777e942f7bf21da5042e389b857
radek-nowak <radeknowak1999@gmail.com>
2024-07-15 22:56:06 +0200

    wip

--- ui/table.go
+++ ui/table.go
@@ -2,10 +2,17 @@ package ui
 
 import (
 	"fmt"
-	todo "go_todo/todo/model"
 	"strings"
 )
 
+// Define Displayable interface
+type Displayable interface {
+	Schema() []string
+	Data() [][]interface{}
+	ColumnWidths() []int
+}
+
+// Define table borders
 const (
 	topLeft      = "╔"
 	topRight     = "╗"
@@ -20,105 +27,129 @@ const (
 	bottomMiddle = "╩"
 )
 
-const (
-	idLength   = 3
-	taskLength = 55
-	doneLength = 5
-)
-
-func Display(tl todo.TodoList) {
-	todos := tl.GetTodos()
+// Display function for Displayable interface
+func Display(d Displayable) {
+	schema := d.Schema()
+	data := d.Data()
+	colWidths := d.ColumnWidths()
 
 	// Display the header
-	fmt.Println(formatHeaderTop())
-	fmt.Println(formatHeaderMiddle())
-	fmt.Println(formatHeaderBottom())
+	fmt.Println(formatHeaderTop(colWidths))
+	fmt.Println(formatHeaderMiddle(schema, colWidths))
+	fmt.Println(formatHeaderBottom(colWidths))
 
 	// Display the rows
-	for i, todo := range todos {
-		taskLines := wrapText(todo.Task, taskLength)
-		for j, line := range taskLines {
+	for i, row := range data {
+		wrappedRows := wrapRow(row, colWidths)
+		for j, wrappedRow := range wrappedRows {
 			if j == 0 {
-				fmt.Printf("%s %-*d %s %-*s %s %-*t %s\n", vertical, idLength, todo.ID, vertical, taskLength, line, vertical, doneLength, todo.Done, vertical)
+				for k, col := range wrappedRow {
+					fmt.Printf("%s %-*s ", vertical, colWidths[k], col)
+				}
+				fmt.Println(vertical)
 			} else {
-				fmt.Printf("%s %-*s %s %-*s %s %-*s %s\n", vertical, idLength, "", vertical, taskLength, line, vertical, doneLength, "", vertical)
+				for k, col := range wrappedRow {
+					fmt.Printf("%s %-*s ", vertical, colWidths[k], col)
+				}
+				fmt.Println(vertical)
 			}
 		}
-		if i < len(todos)-1 {
-			fmt.Println(formatRowSeparator())
+		if i < len(data)-1 {
+			fmt.Println(formatRowSeparator(colWidths))
 		}
 	}
 
 	// Display the footer
-	fmt.Println(formatFooter())
+	fmt.Println(formatFooter(colWidths))
 }
 
-func formatHeaderTop() string {
-	return fmt.Sprintf(
-		"%s%s%s%s%s%s%s",
-		topLeft,
-		strings.Repeat(horizontal, idLength+2),
-		topMiddle,
-		strings.Repeat(horizontal, taskLength+2),
-		topMiddle,
-		strings.Repeat(horizontal, doneLength+2),
-		topRight,
-	)
+func formatHeaderTop(colWidths []int) string {
+	var headerTop strings.Builder
+	headerTop.WriteString(topLeft)
+	for i, width := range colWidths {
+		if i > 0 {
+			headerTop.WriteString(topMiddle)
+		}
+		headerTop.WriteString(strings.Repeat(horizontal, width+2))
+	}
+	headerTop.WriteString(topRight)
+	return headerTop.String()
 }
 
-func formatHeaderMiddle() string {
-	return fmt.Sprintf(
-		"%s %-*s %s %-*s %s %-*s %s",
-		vertical,
-		idLength,
-		"ID",
-		vertical,
-		taskLength,
-		"Task",
-		vertical,
-		doneLength,
-		"Done",
-		vertical,
-	)
+func formatHeaderMiddle(schema []string, colWidths []int) string {
+	var headerMiddle strings.Builder
+	headerMiddle.WriteString(vertical)
+	for i, col := range schema {
+		headerMiddle.WriteString(fmt.Sprintf(" %-*s %s", colWidths[i], col, vertical))
+	}
+	return headerMiddle.String()
 }
 
-func formatHeaderBottom() string {
-	return fmt.Sprintf(
-		"%s%s%s%s%s%s%s",
-		middleLeft,
-		strings.Repeat(horizontal, idLength+2),
-		middle,
-		strings.Repeat(horizontal, taskLength+2),
-		middle,
-		strings.Repeat(horizontal, doneLength+2),
-		middleRight,
-	)
+func formatHeaderBottom(colWidths []int) string {
+	var headerBottom strings.Builder
+	headerBottom.WriteString(middleLeft)
+	for i, width := range colWidths {
+		if i > 0 {
+			headerBottom.WriteString(middle)
+		}
+		headerBottom.WriteString(strings.Repeat(horizontal, width+2))
+	}
+	headerBottom.WriteString(middleRight)
+	return headerBottom.String()
 }
 
-func formatRowSeparator() string {
-	return fmt.Sprintf(
-		"%s%s%s%s%s%s%s",
-		middleLeft,
-		strings.Repeat(horizontal, idLength+2),
-		middle,
-		strings.Repeat(horizontal, taskLength+2),
-		middle,
-		strings.Repeat(horizontal, doneLength+2),
-		middleRight,
-	)
+func formatRowSeparator(colWidths []int) string {
+	var rowSeparator strings.Builder
+	rowSeparator.WriteString(middleLeft)
+	for i, width := range colWidths {
+		if i > 0 {
+			rowSeparator.WriteString(middle)
+		}
+		rowSeparator.WriteString(strings.Repeat(horizontal, width+2))
+	}
+	rowSeparator.WriteString(middleRight)
+	return rowSeparator.String()
 }
 
-func formatFooter() string {
-	return fmt.Sprintf(
-		"%s%s%s%s%s%s%s",
-		bottomLeft,
-		strings.Repeat(horizontal, idLength+2),
-		bottomMiddle,
-		strings.Repeat(horizontal, taskLength+2),
-		bottomMiddle,
-		strings.Repeat(horizontal, doneLength+2),
-		bottomRight,
-	)
+func formatFooter(colWidths []int) string {
+	var footer strings.Builder
+	footer.WriteString(bottomLeft)
+	for i, width := range colWidths {
+		if i > 0 {
+			footer.WriteString(bottomMiddle)
+		}
+		footer.WriteString(strings.Repeat(horizontal, width+2))
+	}
+	footer.WriteString(bottomRight)
+	return footer.String()
+}
+
+func wrapRow(row []interface{}, colWidths []int) [][]string {
+	var wrappedRows [][]string
+	maxLines := 1
+
+	for i, col := range row {
+		colStr := fmt.Sprintf("%v", col)
+		wrapped := wrapText(colStr, colWidths[i])
+		if len(wrapped) > maxLines {
+			maxLines = len(wrapped)
+		}
+		wrappedRows = append(wrappedRows, wrapped)
+	}
+
+	result := make([][]string, maxLines)
+	for i := range result {
+		result[i] = make([]string, len(row))
+		for j := range row {
+			if i < len(wrappedRows[j]) {
+				result[i][j] = wrappedRows[j][i]
+			} else {
+				result[i][j] = ""
+			}
+		}
+	}
+
+	return result
 }
 
 func wrapText(text string, length int) []string {
@@ -135,3 +166,5 @@ func wrapText(text string, length int) []string {
 
 	return wrapped
 }
+
+
