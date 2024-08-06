d24fd5a08e1f0da4eaff931590b9ef3ae52b7c19
radek-nowak <radeknowak1999@gmail.com>
2024-08-06 21:42:31 +0200

    show range cmd flag

--- cmd/showTasks.go
+++ cmd/showTasks.go
@@ -7,20 +7,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var showTaskCmd = &cobra.Command {
-	Use: "show",
-	Short: "shows tasks",
-	Args: cobra.ExactArgs(0),
-	Run: func(cmd *cobra.Command, args []string) {
-		tasks, err := storage.ReadData()
-		if err != nil {
-			panic("Error occured in show task command")
-		}
+const (
+	maxItemsFlagName      = "top"
+	maxItemsFlahShortName = "t"
+)
 
-		ui.Display(*tasks)
+var showTaskCmd = &cobra.Command{
+	Use:     "show",
+	Aliases: []string{"s"},
+	Short:   "shows tasks",
+	Args:    cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		// todo error handling
+		maxItems, _ := cmd.Flags().GetInt(maxItemsFlagName)
+		showTasks(maxItems)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(showTaskCmd)
+	showTaskCmd.PersistentFlags().IntP(maxItemsFlagName, maxItemsFlahShortName, 30, "Shows top x tasks.")
+}
+
+func showTasks(maxItems int) {
+	tasks, err := storage.ReadData(maxItems)
+	if err != nil {
+		panic("Error occured in show task command")
+	}
+
+	ui.Display(tasks)
 }
